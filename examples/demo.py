f8e53f3e5caf857373d7bc28ce2c0c2d25238b33
Amanda Cole <mandyRae@users.noreply.github.com>
2016-06-24 23:48:58 -0400

    Update demo.py

--- examples/demo.py
+++ examples/demo.py
@@ -1,9 +1,10 @@
+'''
 # -------------PYTHONIC-CHARLIE-------------#
+#                  demo.py                  #
 #                                           #
-#       Python class for controlling        #
-#          SparkFun 8x7 LED Array           #
-#           with the Raspberry Pi           #
-#            utilizing RPi.GPIO.            #
+#    Demo of Python class for controlling   #
+#    charlieplexed SparkFun 8x7 LED Array   #
+#           with the Raspberry Pi.          #
 #                                           #
 #  https://www.sparkfun.com/products/13795  #
 #                                           #
@@ -13,6 +14,17 @@
 #                                           #
 #-------------------------------------------#
 
+The MIT License (MIT)
+
+Copyright (c) 2016 Amanda Cole
+
+Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+'''
+#import charlie, of course
 import charlie
 
 #denote the hookup of the GPIO pins, using BCM numbering
